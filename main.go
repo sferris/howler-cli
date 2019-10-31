ddb496a5808e5fb5a632d00fe23dbfaba6010b0d
Shawn Ferris <sferris@localhost.localdomain>
2019-10-30 23:30:38 -0600

    * misc expirementation

--- main.go
+++ main.go
@@ -3,13 +3,18 @@ package main
 import (
   "os"
   "fmt"
-  "time"
+  _"time"
+
+  _"encoding/hex"
 
   howler "github.com/sferris/howler-controller"
 )
 
 func main() {
-  fmt.Println("Hello World!")
+
+/*
+  device := howler.HowlerConfig{}
+*/
 
   device, err := howler.OpenHowlerConfig(0)
   if err != nil {
@@ -17,24 +22,31 @@ func main() {
     os.Exit(1)
   }
 
-  device.SetDefaultLEDColor(howler.Button26,"orange")
-  time.Sleep(time.Second*2)
-
-  device.SetLEDColor(howler.Button("button26"),"red")
-  time.Sleep(time.Second*2)
-
-  device.SetLEDColor(howler.Button("button26"),"green")
-  time.Sleep(time.Second*2)
-
-  device.SetLEDColor(howler.Button("button26"),"blue")
+  button := howler.Button26
 
 /*
-  for i := 0; i < int(howler.MaxButton); i++ {
-    c, _ := device.GetLEDColor(howler.Button(fmt.Sprintf("%d",i)))
-    fmt.Printf("R: %d, G: %d, B: %d\n", c.R, c.G, c.B)
-    time.Sleep(time.Millisecond*500)
-  }
+  fmt.Println("Write")
+  var data = []byte{0xce,0x01,button,0xff,0x00,0xff,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+  device.Write(data)
+
+  fmt.Println("Query")
+  var qry = []byte{0xce,0x08,button,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+  device.Query(qry)
 */
+
+  device.SetDefaultLEDColor(button,"orange")
+
+  device.SetLEDColor(button,"red")
+
+  device.SetLEDColor(button,"green")
+
+  device.SetLEDColor(button,"blue")
+
+  fmt.Println("1st")
+  for i := 0; i < int(howler.ButtonMax); i++ {
+    c, _ := device.GetLEDColor(howler.Button(fmt.Sprintf("%d",i)))
+    fmt.Printf("%d: R: %d, G: %d, B: %d\n", i, c.R, c.G, c.B)
+  }
 
   fw, _ := device.GetFWRelease()
   fmt.Printf("Firmware: %s\n", fw)
@@ -47,10 +59,10 @@ func main() {
     }
     time.Sleep(time.Millisecond*500)
   }
-*/
 
  device.SetInput(howler.InputButton26, howler.ModeJoystick2, 13, howler.ModifierNone)
  time.Sleep(time.Millisecond*500)
 
  device.GetInput(howler.InputButton26)
+*/
 }
