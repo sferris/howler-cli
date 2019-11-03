61362059d869abea8803229efb025167eb9b5d13
Shawn Ferris <sferris@localhost.localdomain>
2019-11-03 08:44:48 -0700

    * Sun 03 Nov 2019 08:44:48 AM MST

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
   "os"
   "fmt"
-  _"time"
+  "time"
 
   _"encoding/hex"
 
@@ -12,73 +12,61 @@ import (
 
 func main() {
 
-/*
-  device := howler.HowlerConfig{}
-*/
+  //howler.DumpDevices()
 
-  device, err := howler.OpenHowlerConfig(0)
+  device, err := howler.OpenDevice(0)
+  defer func() {
+    device.Close()
+  }()
+
   if err != nil {
     fmt.Println(err.Error())
     os.Exit(1)
   }
 
-//  button := howler.Button26
+  button := howler.Button26
 
-/*
-  fmt.Println("Write")
-  var data = []byte{0xce,0x01,button,0xff,0x00,0xff,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
-  device.Write(data)
-
-  fmt.Println("Query")
-  var stmt = []byte{0xce,0x08,button,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
-  device.WriteWithResponse(stmt)
-*/
-
-/*
+  fmt.Printf("Got here: %d\n", 1);
   device.SetDefaultLEDColor(button,"orange")
   time.Sleep(time.Second);
 
+  fmt.Printf("Got here: %d\n", 2);
   device.SetLEDColor(button,"red")
   time.Sleep(time.Second);
 
+  fmt.Printf("Got here: %d\n", 3);
   device.SetLEDColor(button,"green")
   time.Sleep(time.Second);
 
+  fmt.Printf("Got here: %d\n", 4);
   device.SetLEDColor(button,"blue")
   time.Sleep(time.Second);
-*/
 
   for i := 0; i < int(howler.ButtonMax); i++ {
     c, _ := device.GetLEDColor(howler.Buttons(i))
-    fmt.Printf("%d: R: %d, G: %d, B: %d\n", i, c.R, c.G, c.B)
+    fmt.Printf("%03d: %s\n", i, c.ToHexString())
   }
 
   fw, _ := device.GetFWRelease()
-  fmt.Printf("Firmware: %s\n", fw)
+  fmt.Printf("Firmware: %d.%d\n", fw.Major, fw.Minor)
 
-/*
-  for i := 0; i < 100; i++ {
+  for i := 0; i < 200; i++ {
     data, err := device.ReadAccel()
     if err != nil {
       fmt.Println(err.Error())
     } else {
-      fmt.Printf("X: %03d, Y: %03d, Z: %03d\n", data.XAxis, data.YAxis, data.ZAxis)
+      fmt.Println(data.String())
     }
-    time.Sleep(time.Millisecond*250)
   }
-*/
 
-/*
+ device.SetInput(howler.InputButton26, howler.ModeKeyboard, howler.KeyZ, howler.ModifierRightShift)
+ input, err := device.GetInput(howler.InputButton26)
+ input.Dump()
+
   for i := 0; i < int(howler.InputMax); i++ {
-    err := device.GetInput(howler.Inputs(i))
+   _, err := device.GetInput(howler.Inputs(i))
     if err != nil {
       fmt.Println(err.Error())
     }
   }
-
- device.SetInput(howler.InputButton26, howler.ModeJoystick2, 13, howler.ModifierNone)
- time.Sleep(time.Millisecond*500)
-
- device.GetInput(howler.InputButton26)
-*/
 }
